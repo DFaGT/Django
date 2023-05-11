9dec70a86723d5aa738dc3e1a19900c194a5f8a7
DFaGT <dfandgt@gmail.com>
2023-05-11 15:23:52 +0900

    djangotest

---
+++ djangotest/base/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import dbtest
+
+admin.site.register(dbtest)
---
+++ djangotest/base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class dbtest(models.Model):
+	text = models.CharField(max_length=100)
+	number = models.IntegerField()
+
+	def __str__(self):
+		return self.text
