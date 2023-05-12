e9467653626d2cfb603e31c9cd71feb7d9d034a1
DFaGT <dfandgt@gmail.com>
2023-05-12 15:03:37 +0900

    add some model and can load yml

--- djangotest/base/admin.py
+++ djangotest/base/admin.py
@@ -5,4 +5,10 @@ from django.contrib import admin
 
 from .models import dbtest
 
+from .models import student
+
+from .models import parent
+
 admin.site.register(dbtest)
+admin.site.register(student)
+admin.site.register(parent)
--- djangotest/base/models.py
+++ djangotest/base/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
 class dbtest(models.Model):
 	text = models.CharField(max_length=100)
 	number = models.IntegerField()
-
+	
 	def __str__(self):
 		return self.text
+
+class student(models.Model):
+	student_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100)
+	birthday = models.DateField(auto_now=False)
+	gender = models.CharField(max_length=10)
+	sibling = models.BooleanField()
+
+	def __str__(self):
+		return self.name
+
+class parent(models.Model):
+	parent_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100)
+	postal_code = models.PositiveIntegerField()
+	adress = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	phone_number = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
