ef7060f24f5a7c17551d35b3ecb74879634c1eab
DFaGT <dfandgt@gmail.com>
2023-06-22 14:34:51 +0900

    rename japanese and create school form

--- djangotest/base/forms.py
+++ djangotest/base/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Parent
 from .models import Student
+from .models import School
 
 class ParentForm(forms.ModelForm):
     class Meta:
@@ -17,3 +18,30 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'name_kana', 'birthday', 'gender', 'sibling', 'school', 'grade', 'phone_number', 'introducer', 'course']
+
+
+
+class SchoolForm(forms.ModelForm):
+    exam_01 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+    exam_02 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+    exam_03 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+    exam_04 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+    exam_05 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+
+    class Meta:
+        model = School
+        fields = ['name', 'classification', 'city', 'exam_01', 'exam_02', 'exam_03', 'exam_04', 'exam_05']
+
+
+# from django import forms
+
+# class SchoolForm(forms.ModelForm):
+#     exam_01 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+#     exam_02 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+#     exam_03 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+#     exam_04 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+#     exam_05 = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+
+#     class Meta:
+#         model = School
+#         fields = ['name', 'classification', 'city', 'exam_01', 'exam_02', 'exam_03', 'exam_04', 'exam_05']
