9de2c7ce10ccbf5310a77f8d854ff243488ba573
DFaGT <dfandgt@gmail.com>
2023-07-17 19:57:33 +0900

    add how to know choice list

--- djangotest/base/models.py
+++ djangotest/base/models.py
@@ -7,6 +7,15 @@ class GenderChoices(models.TextChoices):
 	Female = "女", "女"
 	Other = "その他", "その他"
 
+class HowToKnowChoices(models.TextChoices):
+	Ans1 = "webサイト", "webサイト"
+	Ans2 = "チラシ", "チラシ"
+	Ans3 = "紹介", "紹介"
+	Ans4 = "兄弟が通塾している", "兄弟が通塾している"
+	Ans5 = "知人からの紹介", "知人からの紹介"
+	Ans6 = "看板を見た", "看板を見た"
+	Ans7 = "その他", "その他"
+
 class ClassChoices(models.TextChoices):
 	Elementary = "小学校", "小学校"
 	JuniorHigh = "中学校", "中学校"
@@ -84,6 +93,7 @@ class School(models.Model):
 
 class Parent(models.Model):
 	STATUS_TYPES = StatusChoices.choices
+	HOWTOKNOW_TYPES = HowToKnowChoices.choices
 	parent_id = models.AutoField(primary_key=True)
 	last_name = models.CharField(max_length=100, null=True)
 	first_name = models.CharField(max_length=100, null=True)
@@ -93,7 +103,7 @@ class Parent(models.Model):
 	phone_number = models.CharField(max_length=100)
 	status = models.CharField(max_length=10, choices=STATUS_TYPES,blank=True)
 	terms = models.BooleanField(null=True)
-	how_to_know = models.CharField(max_length=100, null=True, blank=True)
+	how_to_know = models.CharField(max_length=100,choices=HOWTOKNOW_TYPES, null=True, blank=True)
 
 	def __str__(self):
 		return self.first_name if self.first_name and self.last_name else ''
